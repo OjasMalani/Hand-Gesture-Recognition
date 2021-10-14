1fea722faf2ee82ed14798a2cc21d76ce4acf1bb
OjasMalani <79085373+OjasMalani@users.noreply.github.com>
2021-10-14 15:32:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,32 @@ function speak() {
     var utterThis= new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 }
+
+function check(){
+    img= document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+  }
+  
+  function gotResult(error,results) {
+      if(error){
+          console.error(error);
+      }
+      else{
+          console.log(results);
+          document.getElementById("result_emotion_name").innerHTML=results[0].label;
+          document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+          prediction_1= results[0].label;
+          prediction_2= results[1].label;
+          speak();
+  
+          if(results[0].label== "Thumps Up") {
+              document.getElementById("update_emoji").innerHTML="&#128077";
+          }
+          if(results[0].label== "Amazing") {
+              document.getElementById("update_emoji").innerHTML="&#128076";
+          }
+          if(results[0].label== "Victory") {
+              document.getElementById("update_emoji").innerHTML="&#9996";
+          }
+      }
+  }
